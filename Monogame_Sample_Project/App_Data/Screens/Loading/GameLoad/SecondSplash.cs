4ce1290755bf88d914cb55ac5f63f80cdf85a88c
krolR0609 <nick.arty@gmail.com>
2018-10-10 17:00:27 +0300

    - Add abstract menu class; - Add one menu realization; - Add controller for menu;

---
+++ Monogame_Sample_Project/App_Data/Screens/Loading/GameLoad/SecondSplash.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Monogame_Sample_Project.App_Data.Screens.Menu;
+using Monogame_Sample_Project.Models.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monogame_Sample_Project.App_Data.Screens.LoadScreens.GameLoad
+{
+    public class SecondSplash : GameScreen
+    {
+        Texture2D logoTexture;
+        string path;
+
+        public override void LoadContent()
+        {
+            base.LoadContent();
+            path = "Assets/MonoGameLogo";
+            logoTexture = content.Load<Texture2D>(path);
+        }
+
+        public override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            if (gameTime.TotalGameTime > TimeSpan.FromSeconds(3) || Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                ScreenManager.Instance.LoadGameScreen(new MainMenu());
+            }
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(logoTexture, 
+                new Rectangle(0, 0, (int)ScreenManager.Instance.Dimensions.X, (int)ScreenManager.Instance.Dimensions.Y),
+                Color.White);
+        }
+    }
+}
---
+++ Monogame_Sample_Project/App_Data/Screens/Loading/GameLoad/SplashScreen.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Monogame_Sample_Project.Models.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monogame_Sample_Project.App_Data.Screens.LoadScreens.GameLoad
+{
+    public class SplashScreen : GameScreen
+    {
+        Texture2D logoTexture;
+        string path;
+        public override void LoadContent()
+        {
+            base.LoadContent();
+            path = "Assets/Logo";
+            logoTexture = content.Load<Texture2D>(path);
+        }
+
+        public override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            if (gameTime.TotalGameTime > TimeSpan.FromSeconds(3) || Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                ScreenManager.Instance.LoadGameScreen(new SecondSplash());
+            }
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(logoTexture, 
+                new Rectangle(0, 0, (int)ScreenManager.Instance.Dimensions.X, (int)ScreenManager.Instance.Dimensions.Y),
+                Color.White);
+        }
+    }
+}
